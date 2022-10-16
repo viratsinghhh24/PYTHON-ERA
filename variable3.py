6c9d8546f4feed9cfcb7aecdb20e720448c710fe
Virat <virlove24@gmail.com>
2022-10-16 19:25:05 +0530

    variable3.py

---
+++ variable3.py
@@ -0,0 +1,13 @@
+x , y , z="virat","gaurav","sumit"
+print(x)
+print(y)
+print(z)
+a=b=c="virat"
+print(a)
+print(b)
+print(c)
+names=["virat","gaurav","sumit"]
+p,q,r=names
+print(p)
+print(q)
+print(r)
