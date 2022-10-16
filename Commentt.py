283f4b89df8fec61d629d6756cc6add580b13cf9
Virat <virlove24@gmail.com>
2022-10-16 10:41:40 +0530

    Commentt.py

---
+++ Commentt.py
@@ -0,0 +1,10 @@
+#This is a Comment
+#written in more than just one line 
+print ("hello world")
+print ("My Self Virat Singh")
+print ("Everyrthing ohkay meri jaan")
+"""
+if we want to 
+comment more than one line 
+so we use this
+"""
