ace683c7f8a82c854bd23edfd18bd4594c7ada0f
Virat <virlove24@gmail.com>
2022-10-20 23:55:22 +0530

    multipleanddivision.py

---
+++ multipleanddivision.py
@@ -0,0 +1,10 @@
+a=int(input("enter a no:-"))
+b=int(input("enter b no:-"))
+c=a+b
+print("adding a and b is",c)
+d=a*b
+print("multiplying a and b",d)
+e=a/b
+print("division of a and b",e)
+f=a-b
+print("Substraction of a and b",f)
